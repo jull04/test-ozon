61f2dae56095b27580f1dd9c8e53a9a08d1b6e36
Julia Khabibova <julia.khabibova@mail.ru>
2026-09-03 18:24:45 +0300

    bug fix

--- index.js
+++ index.js
@@ -2,54 +2,45 @@ class ProgressBlock {
   constructor(element, options = {}) {
     this.element = document.querySelector(element);
 
-    this.settings = {
-      fillSelector: options.fillSelector || ".progress__fill",
-      animatedClass: options.animatedClass || 'progress__block--animated',
-      hiddenClass: options.hiddenClass || 'progress__block--hidden',   
-      ...options,
+    const defaults = { fillClass: 'progress__fill',
+    animatedClass: 'is-animated', 
+    hiddenClass: 'is-hidden' 
     };
 
-    this.fillElement = this.element.querySelector(this.settings.fillSelector);
+    this.settings = { ...defaults, ...options };
+
+    this.fillElement = this.element.querySelector(`.${this.settings.fillClass}`);
 
     this.state = {
       value: 0,
       isAnimated: false,
       isHidden: false,
     };
+
+    // Длина окружности (для круга r=45) = 2πr ≈ 282.74 / округляем до 283 для удобства 
+    this.CIRCLE_LENGTH = 283;
   }
 
   _updateProgress() {
-    if (!this.state.isAnimated) {
-      const offset = 283 - (this.state.value / 100) * 283;
-      this.fillElement.style.strokeDashoffset = offset;
-    }
+    const offset = this.CIRCLE_LENGTH - (this.state.value / 100) * this.CIRCLE_LENGTH; 
+    this.fillElement.style.strokeDashoffset = `${offset}px`;
   }
 
   setValue(value) {
-    const newValue = Math.max(0, Math.min(100, Number(value)));
-    this.state.value = newValue;
-    this._updateProgress();
-
+    this.state.value = Math.max(0, Math.min(100, Number(value))); 
+    this._updateProgress(); 
     return this;
   }
 
-  setAnimated(state) {
-    this.state.isAnimated = Boolean(state);
-
-    if (this.state.isAnimated) {
-      this.element.classList.add(this.settings.animatedClass);
-    } else {
-      this.element.classList.remove(this.settings.animatedClass);
-      this._updateProgress();
-    }
-
-    return this;
-  }
+  setAnimated(state) { this.state.isAnimated = Boolean(state); 
+    if (state) { this.element.classList.add(this.settings.animatedClass); } 
+    else { this.element.classList.remove(this.settings.animatedClass); } 
+  return this; }
 
   setHidden(state) {
     this.state.isHidden = Boolean(state);
 
-    if (this.state.isHidden) {
+    if (state) {
       this.element.classList.add(this.settings.hiddenClass);
     } else {
       this.element.classList.remove(this.settings.hiddenClass);
